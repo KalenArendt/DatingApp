c48412365678e89947eb0fcb61808743a14b2c16
Kalen Arendt <dev.KalenArendt@Gmail.com>
2023-01-02 16:48:41 -0600

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -3,18 +3,31 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+/* -------------------------------------------------
+**** Add services to the container
+    - order is not too important
+------------------------------------------------- */
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(
-    opt => {
+    opt =>
+    {
         opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
     }
 );
+builder.Services.AddCors();
 
 var app = builder.Build(); // anything before this is considered our services container
 
-// Configure the HTTP request pipeline.
+/* -------------------------------------------------
+**** Configure the HTTP request pipeline. (Middleware)
+    - Order matters
+------------------------------------------------- */
+
+app.UseCors(
+    builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
+);
+
 app.MapControllers();
 
 app.Run();

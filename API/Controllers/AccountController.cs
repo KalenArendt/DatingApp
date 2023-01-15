cf15dfa03bbf5f7dfd673baf764a3bc9d473cc8f
Kalen Arendt <dev.KalenArendt@Gmail.com>
2023-01-14 21:09:20 -0600

    End of section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,87 @@
+using System.Security.Cryptography;
+using System.Text;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        private readonly ITokenService _tokenService;
+
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            _context = context;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost("register")] // POST api/account/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if (await UserExists(registerDto.Username))
+            {
+                return BadRequest("Username is taken");
+            }
+
+            using var hmac = new HMACSHA512();
+
+            var user = new AppUser
+            {
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return new UserDto
+            {
+                Username = registerDto.Username,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            // get the user by username
+            var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDto.Username);
+
+            if (user == null)
+            {
+                return Unauthorized($"invalid {nameof(loginDto.Username)}");
+            }
+
+            // check if the password is correct
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != loginDto.Password[i])
+                {
+                    return Unauthorized($"invalid {nameof(loginDto.Password)}");
+                }
+            }
+
+            return new UserDto
+            {
+                Username = loginDto.Username,
+                Token = _tokenService.CreateToken(user)
+            };
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,23 +1,16 @@
-using API.Data;
-using Microsoft.EntityFrameworkCore;
+using API.Extentions;
 
-var builder = WebApplication.CreateBuilder(args);
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 /* -------------------------------------------------
 **** Add services to the container
     - order is not too important
 ------------------------------------------------- */
-
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(
-    opt =>
-    {
-        opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-    }
-);
-builder.Services.AddCors();
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-var app = builder.Build(); // anything before this is considered our services container
+WebApplication app = builder.Build(); // anything before this is considered our services container
 
 /* -------------------------------------------------
 **** Configure the HTTP request pipeline. (Middleware)
@@ -25,9 +18,16 @@ var app = builder.Build(); // anything before this is considered our services co
 ------------------------------------------------- */
 
 app.UseCors(
-    builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200")
+    builder
+    => builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("https://localhost:4200")
 );
 
 app.MapControllers();
+
+app.UseAuthentication();    // do you have a valid token                        -- do you have valid id?
+app.UseAuthorization();     // what are you allowed to do with the said token   -- does your id say you're 18+? 
 
 app.Run();
